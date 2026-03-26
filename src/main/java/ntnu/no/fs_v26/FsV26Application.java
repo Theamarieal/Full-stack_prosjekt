d7323527a0ae3564484e3048921902464f9d68bb
Thea Lønvik <tmal339@gmail.com>
2026-03-26 14:27:23 +0100

    First commit of Spring Boot project

---
+++ src/main/java/ntnu/no/fs_v26/FsV26Application.java
@@ -0,0 +1,13 @@
+package ntnu.no.fs_v26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsV26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsV26Application.class, args);
+	}
+
+}
---
+++ src/test/java/ntnu/no/fs_v26/FsV26ApplicationTests.java
@@ -0,0 +1,13 @@
+package ntnu.no.fs_v26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsV26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
