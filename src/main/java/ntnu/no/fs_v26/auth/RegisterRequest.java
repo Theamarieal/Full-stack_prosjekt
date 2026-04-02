c387ede960eaea9da8d2b4c8105923177042dd94
Ida Aspenes-Isaksen <idaaspenesisaksen@gmail.com>
2026-04-02 10:27:33 +0200

    feat(backend): update User model and authentication DTOs

--- src/main/java/ntnu/no/fs_v26/auth/RegisterRequest.java
+++ src/main/java/ntnu/no/fs_v26/auth/RegisterRequest.java
@@ -4,14 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ntnu.no.fs_v26.model.Role; 
+import ntnu.no.fs_v26.model.Role;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
-  private String email;
-  private String password;
-  private Role role;
+    private String email;
+    private String password;
+    private Role role;
+    private Long organizationId; // Denne manglet!
 }
